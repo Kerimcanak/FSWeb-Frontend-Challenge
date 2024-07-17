7a0d8b54e7810d1ed23263203455c249b70f683b
Kerim <kerimcanak@gmail.com>
2024-07-17 09:57:20 +0300

    Update LanguageSwitcher.jsx

--- kisiselWebsite/src/assets/LanguageSwitcher.jsx
+++ kisiselWebsite/src/assets/LanguageSwitcher.jsx
@@ -1,17 +1,29 @@
 import { useContext, useState } from 'react';
 import { LanguageContext } from './LanguageContext';
+import axios from 'axios';
 
 function LanguageSwitcher() {
   const { dispatch } = useContext(LanguageContext);
   const [language, setLanguage] = useState('en');
 
-  const toggleLanguage = () => {
-    if (language === 'en') {
-      dispatch({ type: 'SET_LANGUAGE', payload: 'tr' });
-      setLanguage('tr');
-    } else {
-      dispatch({ type: 'SET_LANGUAGE', payload: 'en' });
-      setLanguage('en');
+  const toggleLanguage = async () => {
+    try {
+      const response = await axios.post('https://reqres.in/api/workintech', {
+        language: language === 'en' ? 'tr' : 'en',
+      });
+      const audio = new Audio(
+        response.status === 200
+          ? 'https://upload.wikimedia.org/wikipedia/commons/6/68/Bicycle-bell-1.wav'
+          : 'https://upload.wikimedia.org/wikipedia/commons/8/83/Bicycle-bell-3.wav'
+      );
+      audio.play();
+      dispatch({
+        type: 'SET_LANGUAGE',
+        payload: language === 'en' ? 'tr' : 'en',
+      });
+      setLanguage(language === 'en' ? 'tr' : 'en');
+    } catch (error) {
+      console.error(error);
     }
   };
 
