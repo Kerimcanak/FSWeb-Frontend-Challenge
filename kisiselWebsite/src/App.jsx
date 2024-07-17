645546613d8e9268a67c40814ea1b4e8a63b0de7
Kerim <kerimcanak@gmail.com>
2024-07-17 09:25:59 +0300

    design is done

--- kisiselWebsite/src/App.jsx
+++ kisiselWebsite/src/App.jsx
@@ -165,12 +165,12 @@ function App() {
   <div className={`max-w-md mx-auto bg-${checked ? 'stone-800' : 'white'} rounded-xl shadow-md overflow-hidden md:max-w-2xl`}>
     <div className="md:flex">
       <div className="md:shrink-0">
-        <img className="h-48 w-full object-cover md:h-full md:w-48" src="/img/building.jpg" alt="Modern building architecture" />
+        <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://www.biggerbolderbaking.com/wp-content/uploads/2021/02/New-York-Style-Pizza-Thumbnail1-scaled.jpg" alt="Modern building architecture" />
       </div>
       <div className="p-8">
-        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
-        <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accommodation for your team</a>
-        <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">fsweb-s7-pizza</div>
+        <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Github</a>
+        <p className="mt-2 text-slate-500">Commerce website.</p>
       </div>
     </div>
   </div>
@@ -178,12 +178,12 @@ function App() {
   <div className={`max-w-md mx-auto ${checked ? 'bg-stone-800' : 'bg-white'} rounded-xl shadow-md overflow-hidden md:max-w-2xl`}>
     <div className="md:flex">
       <div className="md:shrink-0">
-        <img className="h-48 w-full object-cover md:h-full md:w-48" src="/img/building.jpg" alt="Modern building architecture" />
+        <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://i.pinimg.com/originals/60/7e/2d/607e2df78032070e9be1006be22f007f.png" alt="Modern building architecture" />
       </div>
       <div className="p-8">
-        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
-        <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accommodation for your team</a>
-        <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">netflixMockup</div>
+        <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Github</a>
+        <p className="mt-2 text-slate-500">Streaming website.</p>
       </div>
     </div>
   </div>
