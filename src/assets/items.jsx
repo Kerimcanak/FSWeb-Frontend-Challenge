1ef43661f55556f8ad8f0083039cda1822755b46
Kerim <kerimcanak@gmail.com>
2024-07-18 23:00:43 +0300

    logos moved to accurate file system

--- src/assets/items.jsx
+++ src/assets/items.jsx
@@ -1,38 +1,33 @@
-import logo1 from "./logo1.png";
-import logo2 from "./logo2.png";
-import logo3 from "./logo3.png";
-import logo4 from "./logo4.png";
-import logo5 from "./logo5.png";
-import logo6 from "./logo6.png";
+
 
 const items = [
   {
-    image: "./vite.svg",
+    image: "./logo1.png",
     title: "Figma",
     description: "VGE/Editor program."
   },
   {
-    image: logo2,
+    image: "./logo2.png",
     title: "Redux",
     description: "State management."
   },
   {
-    image: logo3,
+    image: "./logo3.png",
     title: "VS Code",
     description: "ide."
   },
   {
-    image: logo4,
+    image: "./logo4.png",
     title: "React",
     description: "Javascript Framework."
   },
   {
-    image: logo5,
+    image: "./logo5.png",
     title: "Node.js",
     description: "Server side development."
   },
   {
-    image: logo6,
+    image: "./logo6.png",
     title: "Javascript",
     description: "Web development."
   }
